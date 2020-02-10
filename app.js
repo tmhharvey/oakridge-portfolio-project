8985e0ef4b2c40e4489951680bf1b965dbaf6bbd
Taylor Harvey <tmhharvey@gmail.com>
2020-02-10 15:38:16 -0500

    CWP changed to 0

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var path = require("path");
+
+
 var ejs = require("ejs");
 var forceSsl = require('force-ssl-heroku');
 
@@ -15,13 +17,15 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 
 app.use("/", indexRouter);
 
 var port = process.env.PORT || 3000;
 
- 
+
 app.listen(port, () => console.log('Server listening on port ' + port));
 
 module.exports = app;
